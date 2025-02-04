1d93f0c45af832c9ae2b5919f6049b3b5d25c2dc
Fournet Enzo <enzofournet31@gmail.com>
2025-02-04 14:13:13 +0100

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,4 +9,8 @@ import { CompetencesComponent } from '../competences/competence/competence.compo
 })
 export class HomeComponent {
   protected readonly CompetencesComponent = CompetencesComponent;
+  age: number = Math.floor(
+    (new Date().getTime() - new Date('2004-09-13').getTime()) /
+      (1000 * 60 * 60 * 24 * 365.25),
+  );
 }
